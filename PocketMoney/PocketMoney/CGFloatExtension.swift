ef41869497dcac4cabc1eeb137d867eaeba18a9c
danielbogomazov <danielbogomazov@gmail.com>
2017-11-20 22:27:11 -0500

    Created CGFloat extension for radian/degree conversion

---
+++ PocketMoney/PocketMoney/CGFloatExtension.swift
@@ -0,0 +1,14 @@
+//
+//  CGFloatExtension.swift
+//  PocketMoney
+//
+//  Created by Daniel on 2017-11-20.
+//  Copyright © 2017 Daniel Bogomazov. All rights reserved.
+//
+
+import Foundation
+
+extension CGFloat {
+    var degreesToRadians: CGFloat { return self * .pi / 180 }
+    var radiansToDegrees: CGFloat { return self * 180 / .pi }
+}
--- PocketMoney/PocketMoney/ProgressView.swift
+++ PocketMoney/PocketMoney/ProgressView.swift
@@ -140,7 +140,3 @@ class ProgressView: UIView {
     }
 }
 
-extension CGFloat {
-    var degreesToRadians: CGFloat { return self * .pi / 180 }
-    var radiansToDegrees: CGFloat { return self * 180 / .pi }
-}
